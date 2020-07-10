7e8572b768d28f2b4d03482bf65a4bede227cbe5
programatorroka <lukasprokop547@gmail.com>
2020-07-10 10:02:29 +0200

    zmeny

--- AddressBookSQLite/src/AdressBookSQLite/FXMLTableViewController.java
+++ AddressBookSQLite/src/AdressBookSQLite/FXMLTableViewController.java
@@ -5,8 +5,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 
 public class FXMLTableViewController  {
 
@@ -14,7 +16,7 @@ public class FXMLTableViewController  {
     @FXML private TextField firstNameField;
     @FXML private TextField lastNameField;
     @FXML private TextField emailField;
-    @FXML SQLiteJDBC sqlConnector;
+    @FXML SQLiteJDBC jdbc;
 
 
     @FXML protected void initialize(){
@@ -25,29 +27,45 @@ public class FXMLTableViewController  {
     //TODO: Add remove person functionality - https://www.sqlitetutorial.net/sqlite-delete/
     //TODO: Add update person functionality - https://www.sqlitetutorial.net/sqlite-update/
 
-
     @FXML protected void addPerson(ActionEvent event) {
         ObservableList<Person> data = tableView.getItems();
 
         data.add(new Person(firstNameField.getText(), lastNameField.getText(), emailField.getText()));
-        sqlConnector.insertPerson(lastNameField.getText(), firstNameField.getText(), emailField.getText());
+        jdbc.insertPerson(lastNameField.getText(), firstNameField.getText(), emailField.getText());
 
         firstNameField.setText("");
         lastNameField.setText("");
         emailField.setText("");
     }
 
+    @FXML protected void removePerson(){
+        Person p = tableView.getItems().get(tableView.getSelectionModel().getSelectedIndex());
+        tableView.getItems().remove(p);
+        jdbc.removePerson(p);
+    }
 
+    @FXML protected void updatePerson(){
+        Person nova = new Person(lastNameField.getText(), firstNameField.getText(), emailField.getText());
+        Person stara = tableView.getSelectionModel().getSelectedItem();
+
+        int i = tableView.getSelectionModel().getSelectedIndex();
+        tableView.getItems().set(i, nova);
+        jdbc.updatePerson(nova, stara);
+
+        firstNameField.setText("");
+        lastNameField.setText("");
+        emailField.setText("");
+    }
 
     @FXML protected void connectDatabase() {
-        sqlConnector = new SQLiteJDBC();
+        jdbc = new SQLiteJDBC();
     }
 
     @FXML protected void populateTableView() {
         ObservableList<Person> data = tableView.getItems();
         ResultSet resultSet = null;
         try {
-            resultSet = sqlConnector.getResultSet();
+            resultSet = jdbc.getResultSet();
 
         while(resultSet.next()) {
             String lastName = resultSet.getString("lastname");
--- AddressBookSQLite/src/AdressBookSQLite/SQLiteJDBC.java
+++ AddressBookSQLite/src/AdressBookSQLite/SQLiteJDBC.java
@@ -1,9 +1,11 @@
 package AdressBookSQLite;
+
 import java.sql.*;
 
 public class SQLiteJDBC {
 
-    public SQLiteJDBC(){
+
+    public SQLiteJDBC() {
         Connection c = null;
         Statement stmt = null;
 
@@ -13,16 +15,16 @@ public class SQLiteJDBC {
             System.out.println("Opened database successfully");
 
             stmt = c.createStatement();
-            String createTable = "CREATE TABLE IF NOT EXISTS person" +
-                           " (lastname  TEXT    NOT NULL, " +
-                           " firstname  TEXT    NOT NULL, " +
-                           " email  TEXT    NOT NULL)";
-            stmt.executeUpdate(createTable);
+            String sql = "CREATE TABLE IF NOT EXISTS person" +
+                    " (lastname  TEXT    NOT NULL, " +
+                    " firstname  TEXT    NOT NULL, " +
+                    " email  TEXT    NOT NULL)";
+            stmt.executeUpdate(sql);
             stmt.close();
             c.close();
 
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
 
         }
         System.out.println("Table created successfully");
@@ -40,30 +42,49 @@ public class SQLiteJDBC {
     }
 
 
-
-
     public void insertPerson(String lastName, String firstName, String email) {
         try {
-            PreparedStatement preparedStatement;
-            Connection connection =  this.getConnection();
-            String sqlInsert = "INSERT INTO person (lastName, firstName, email)" +
-                    "VALUES (?,?,?)";
-            preparedStatement = connection.prepareStatement(sqlInsert);
-            preparedStatement.setString(1,lastName);
-            preparedStatement.setString(2,firstName);
-            preparedStatement.setString(3,email);
-            preparedStatement.executeUpdate();
-
+            String sqlInsert = "INSERT INTO person (lastName, firstName, email) VALUES (?,?,?)";
+            executeAsPreparedStatement(sqlInsert, new Person(lastName, firstName, email));
         } catch (SQLException e) {
-            System.err.println( e.getMessage() );
+            System.err.println(e.getMessage());
         }
     }
 
     public ResultSet getResultSet() throws SQLException {
-            String query = "SELECT * FROM person";
-            ResultSet resultSet = this.getConnection().createStatement().executeQuery(query);
-            return resultSet;
+        String query = "SELECT * FROM person";
+        ResultSet resultSet = this.getConnection().createStatement().executeQuery(query);
+        return resultSet;
+    }
 
+    public void removePerson(Person p) {
+        try {
+            String query = "DELETE FROM person WHERE lastName = ? AND firstName = ? AND email = ?";
+            executeAsPreparedStatement(query, p);
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private void executeAsPreparedStatement(String query, Person p) throws SQLException {
+        Connection connection = this.getConnection();
+        PreparedStatement preparedStatement;
+        preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, p.getLastName());
+        preparedStatement.setString(2, p.getFirstName());
+        preparedStatement.setString(3, p.getEmail());
+        preparedStatement.executeUpdate();
+    }
+
+    public void updatePerson(Person nova, Person stara) {
+        try {
+            String query = "UPDATE person SET lastName = ?, firstName = ?, email = ? " +
+                    "WHERE lastName = \"" + stara.getLastName() + "\" AND firstName = \"" + stara.getFirstName() + "\"" +
+                    " AND email = \"" + stara.getEmail() + "\"";
+            executeAsPreparedStatement(query, nova);
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
 }
